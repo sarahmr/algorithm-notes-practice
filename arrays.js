aa75eb46570436c5f4f7afbd42571867c812c2cf
Sarah Rose <sarahrosesmr@gmail.com>
2020-10-10 21:29:47 -0400

    buy and sell stock array

--- arrays.js
+++ arrays.js
@@ -15,3 +15,41 @@ let removeDuplicates = function(nums) {
   }
   return i + 1;
 };
+
+// best time to buy and sell stock
+
+// solution 1: brute force
+class Solution {
+  maxProfit = (prices) => {
+    return this.calculate(prices, 0);
+  }
+  
+  calculate = (prices, s) => {
+    if (s >= prices.length) {
+      return 0;
+    }
+    
+    let max = 0;
+    for (let start = s; start < prices.length; start++) {
+      let maxprofit = 0;
+      for (let i = start + 1; i < prices.length; i++) {
+        if (prices[start] < prices[i]) {
+          let profit = this.calculate(prices, i + 1) + prices[i] - prices[start];
+          if (profit > maxprofit) {
+            maxprofit = profit;
+          }
+        }
+        if (maxprofit > max) {
+          max = maxprofit;
+        }
+      }
+    }
+  return max;      
+  }
+}
+
+let maxProfit = function(prices) {
+  let solution = new Solution;
+  return solution.maxProfit(prices)
+}
+
